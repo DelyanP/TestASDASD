1580a111e63038aaace843544012b3f33706c403
DelyanP <peev@isenselabs.com>
2016-06-16 11:38:45 +0300

    recommit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://delyan.isenselabs.com/cliantrolive/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://delyan.isenselabs.com/cliantrolive/');
+
+// DIR
+define('DIR_APPLICATION', '/home/delyanisenselabs/public_html/cliantrolive/catalog/');
+define('DIR_SYSTEM', '/home/delyanisenselabs/public_html/cliantrolive/system/');
+define('DIR_IMAGE', '/home/delyanisenselabs/public_html/cliantrolive/image/');
+define('DIR_LANGUAGE', '/home/delyanisenselabs/public_html/cliantrolive/catalog/language/');
+define('DIR_TEMPLATE', '/home/delyanisenselabs/public_html/cliantrolive/catalog/view/theme/');
+define('DIR_CONFIG', '/home/delyanisenselabs/public_html/cliantrolive/system/config/');
+define('DIR_CACHE', '/home/delyanisenselabs/public_html/cliantrolive/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/delyanisenselabs/public_html/cliantrolive/system/storage/download/');
+define('DIR_LOGS', '/home/delyanisenselabs/public_html/cliantrolive/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/delyanisenselabs/public_html/cliantrolive/system/storage/modification/');
+define('DIR_UPLOAD', '/home/delyanisenselabs/public_html/cliantrolive/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'delyanis_admin');
+define('DB_PASSWORD', 'Delyan@2');
+define('DB_DATABASE', 'delyanis_cliantro');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc22_');
